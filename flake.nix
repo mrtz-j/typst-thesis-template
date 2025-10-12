f3131694d348927ee38bcc27989bc687bd50fbe3
Moritz Jörg <mail@mrtz.xyz>
2025-10-12 08:26:37 +0200

    fix: Switch to nixpkgs typship

--- flake.nix
+++ flake.nix
@@ -66,6 +66,7 @@
 
               # Utils
               typos
+              typship
               sd
             ];
 
--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -3,5 +3,4 @@ rec {
   xcharter = pkgs.callPackage ./xcharter.nix { };
   glossarium = pkgs.callPackage ./glossarium.nix { };
   thesis = pkgs.callPackage ./thesis.nix { inherit xcharter glossarium; };
-  typship = pkgs.callPackage ./typship.nix { };
 }
