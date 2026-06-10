e988f4717e16e36db39ec31436f0c5baa20b27df
Moritz Jörg <moritz.jorg@oceanbox.io>
2026-06-10 12:50:32 +0200

    devel: Remove now deprecated Garnix CI

--- flake.nix
+++ flake.nix
@@ -1,9 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = "https://cache.garnix.io";
-    extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
